6a24018d144faa0a9c94824b8ebd269927a4d644
Quareex <166931013+Quareex@users.noreply.github.com>
2025-01-03 12:22:06 +0100

    Add files via upload

--- src/main/java/com/example/tictactoe/Controller.java
+++ src/main/java/com/example/tictactoe/Controller.java
@@ -1,16 +1,19 @@
 package com.example.tictactoe;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
 
 import java.io.IOException;
 
 public class Controller {
-
     private Stage stage;
     private Scene scene;
 
@@ -47,6 +50,43 @@ public class Controller {
         Parent root = loadFXML("scoreboard.fxml");
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToGamePvP(ActionEvent event) throws IOException {
+        Parent root = loadFXML("game-board-pvp.fxml");
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+    public void switchToGameBotNormal(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("game-board-bot.fxml"));
+        Parent root = loader.load();
+
+        botLogic botLogicController = loader.getController();
+        String difficulty = "Normal";
+        botLogicController.setDifficulty(difficulty);
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToGameBotHard(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("game-board-bot.fxml"));
+        Parent root = loader.load();
+
+        botLogic botLogicController = loader.getController();
+        String difficulty = "Hard" ;
+        botLogicController.setDifficulty(difficulty);
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/main/java/com/example/tictactoe/botLogic.java
@@ -0,0 +1,222 @@
+package com.example.tictactoe;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.ResourceBundle;
+import java.net.URL;
+import java.util.Arrays;
+
+public class botLogic implements Initializable {
+    @FXML
+    private Button button00;
+
+    @FXML
+    private Button button01;
+
+    @FXML
+    private Button button02;
+
+    @FXML
+    private Button button10;
+
+    @FXML
+    private Button button11;
+
+    @FXML
+    private Button button12;
+
+    @FXML
+    private Button button20;
+
+    @FXML
+    private Button button21;
+
+    @FXML
+    private Button button22;
+
+    @FXML
+    private Text winnerText;
+
+    private int playerTurn = 0;
+    ArrayList<Button> buttons;
+    private String difficulty;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resource) {
+        buttons = new ArrayList<>(Arrays.asList(button00, button01, button02, button10, button11, button12, button20, button21, button22));
+        buttons.forEach(button -> {
+            setupButton(button);
+            button.setFocusTraversable(false);
+        });
+    }
+
+    public void setDifficulty(String difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    @FXML
+    void restart(ActionEvent event) {
+        buttons.forEach(this::resetButton);
+        winnerText.setText("Kółko i krzyżyk");
+        playerTurn = 0;
+    }
+
+    public void resetButton(Button button) {
+        button.setDisable(false);
+        button.setText("");
+    }
+
+    private void setupButton(Button button) {
+        button.setOnMouseClicked(event -> {
+            setPlayerSymbol(button);
+            button.setDisable(true);
+            checkWinCondition();
+
+            if (playerTurn % 2 == 1) {
+                botMove();
+            }
+        });
+    }
+
+    public void setPlayerSymbol(Button button) {
+        if (playerTurn % 2 == 0) {
+            button.setText("X");
+        } else {
+            button.setText("O");
+        }
+        playerTurn++;
+    }
+
+    private void botMove() {
+        if (difficulty.equals("normal")) {
+            randomMove();
+        } else {
+            smartMove();
+        }
+        checkWinCondition();
+    }
+
+    private void randomMove() {
+        Random rand = new Random();
+        ArrayList<Button> availableButtons = new ArrayList<>();
+        for (Button button : buttons) {
+            if (button.getText().isEmpty()) {
+                availableButtons.add(button);
+            }
+        }
+        if (!availableButtons.isEmpty()) {
+            Button randomButton = availableButtons.get(rand.nextInt(availableButtons.size()));
+            randomButton.setText("O");
+            randomButton.setDisable(true);
+            playerTurn++;
+        }
+    }
+
+    private void smartMove() {
+        for (Button button : buttons) {
+            if (button.getText().isEmpty()) {
+                button.setText("O");
+                if (checkForWin("O")) {
+                    return;
+                }
+                button.setText("");
+            }
+        }
+
+        // Jeśli nie może wygrać, blokuje gracza
+        for (Button button : buttons) {
+            if (button.getText().isEmpty()) {
+                button.setText("X");
+                if (checkForWin("X")) {
+                    button.setText("O");
+                    button.setDisable(true);
+                    playerTurn++;
+                    return;
+                }
+                button.setText("");
+            }
+        }
+
+        // Jeśli nic nie można zrobić, wykonuje ruch losowy
+        randomMove();
+    }
+
+    private boolean checkForWin(String symbol) {
+        for (int i = 0; i < 8; i++) {
+            String check = switch (i) {
+                case 0 -> button00.getText() + button01.getText() + button02.getText();
+                case 1 -> button10.getText() + button11.getText() + button12.getText();
+                case 2 -> button20.getText() + button21.getText() + button22.getText();
+                case 3 -> button00.getText() + button10.getText() + button20.getText();
+                case 4 -> button01.getText() + button11.getText() + button21.getText();
+                case 5 -> button02.getText() + button12.getText() + button22.getText();
+                case 6 -> button00.getText() + button11.getText() + button22.getText();
+                case 7 -> button02.getText() + button11.getText() + button20.getText();
+                default -> null;
+            };
+            if (check.equals(symbol + symbol + symbol)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void checkWinCondition() {
+        for (int i = 0; i < 8; i++) {
+            String check = switch (i) {
+                case 0 -> button00.getText() + button01.getText() + button02.getText();
+                case 1 -> button10.getText() + button11.getText() + button12.getText();
+                case 2 -> button20.getText() + button21.getText() + button22.getText();
+                case 3 -> button00.getText() + button10.getText() + button20.getText();
+                case 4 -> button01.getText() + button11.getText() + button21.getText();
+                case 5 -> button02.getText() + button12.getText() + button22.getText();
+                case 6 -> button00.getText() + button11.getText() + button22.getText();
+                case 7 -> button02.getText() + button11.getText() + button20.getText();
+                default -> null;
+            };
+
+            if (check.equals("XXX")) {
+                winnerText.setText("Wygrał krzyżyk");
+                disableAllButtons();
+                return;
+            } else if (check.equals("OOO")) {
+                winnerText.setText("Wygrał bot");
+                disableAllButtons();
+                return;
+            }
+
+        }
+
+        boolean isDraw = buttons.stream().allMatch(button -> !button.getText().isEmpty());
+        if (isDraw) {
+            winnerText.setText("Remis");
+            disableAllButtons();
+        }
+
+    }
+
+    private void disableAllButtons() {
+        buttons.forEach(button -> button.setDisable(true));
+    }
+
+    @FXML
+    public void switchToMenu(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("main-menu.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
