89c1bb7b3e91a50322917811058e1e0fd51d1a68
jeheke <kacper.nyga123@gmail.com>
2025-01-10 13:18:29 +0100

    .

--- src/main/java/com/example/tictactoe/Controller.java
+++ src/main/java/com/example/tictactoe/Controller.java
@@ -7,90 +7,60 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
 import java.io.IOException;
 
 public class Controller {
     private Stage stage;
     private Scene scene;
 
-    private double windowWidth;
-    private double windowHeight;
-
-    public void setStage(Stage stage) {
-        this.stage = stage;
-    }
-
     private Parent loadFXML(String fxmlFile) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlFile));
         return loader.load();
     }
 
-    public void saveWindowSize() {
-        if (stage != null) {
-            windowWidth = stage.getWidth();
-            windowHeight = stage.getHeight();
-        }
-    }
-
-    public void restoreWindowSize() {
-        if (stage != null) {
-            stage.setWidth(windowWidth);
-            stage.setHeight(windowHeight);
-        }
-    }
-
     public void switchToMode(ActionEvent event) throws IOException {
-        saveWindowSize();
         Parent root = loadFXML("mode-selection.fxml");
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
-        restoreWindowSize();
     }
 
     public void switchToMenu(ActionEvent event) throws IOException {
-        saveWindowSize();
         Parent root = loadFXML("main-menu.fxml");
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
-        restoreWindowSize();
     }
 
     public void switchToDifficulty(ActionEvent event) throws IOException {
-        saveWindowSize();
         Parent root = loadFXML("difficulty.fxml");
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
-        restoreWindowSize();
     }
 
     public void switchToScoreboard(ActionEvent event) throws IOException {
-        saveWindowSize();
         Parent root = loadFXML("scoreboard.fxml");
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
-        restoreWindowSize();
     }
 
     public void switchToGamePvP(ActionEvent event) throws IOException {
-        saveWindowSize();
         Parent root = loadFXML("game-board-pvp.fxml");
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
-        restoreWindowSize();
+
     }
 
     public void switchToGameBotNormal(ActionEvent event) throws IOException {
-        saveWindowSize();
         FXMLLoader loader = new FXMLLoader(getClass().getResource("game-board-bot.fxml"));
         Parent root = loader.load();
 
@@ -98,27 +68,24 @@ public class Controller {
         String difficulty = "Normal";
         botLogicController.setDifficulty(difficulty);
 
-        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
-        restoreWindowSize();
     }
 
     public void switchToGameBotHard(ActionEvent event) throws IOException {
-        saveWindowSize();
         FXMLLoader loader = new FXMLLoader(getClass().getResource("game-board-bot.fxml"));
         Parent root = loader.load();
 
         botLogic botLogicController = loader.getController();
-        String difficulty = "Hard";
+        String difficulty = "Hard" ;
         botLogicController.setDifficulty(difficulty);
 
-        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
-        restoreWindowSize();
     }
 
     public void exit(ActionEvent event) {
--- src/main/java/com/example/tictactoe/Menu.java
+++ src/main/java/com/example/tictactoe/Menu.java
@@ -4,9 +4,9 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
-import java.util.Objects;
 
 public class Menu extends Application {
     @Override
@@ -14,14 +14,8 @@ public class Menu extends Application {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("main-menu.fxml"));
         Scene scene = new Scene(loader.load());
 
-        Controller controller = loader.getController();
-        controller.setStage(stage);
-        controller.saveWindowSize();
-
-        String css = Objects.requireNonNull(getClass().getResource("style.css")).toExternalForm();
-        scene.getStylesheets().add(css);
-
         stage.setTitle("Kółko i krzyżyk");
+        stage.initStyle(StageStyle.UNDECORATED);
         stage.setScene(scene);
         stage.show();
     }
